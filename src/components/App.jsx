399e28090b542346580b015032854074bad42cfe
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-21 18:34:45 -0800

    Add Name Input field to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,42 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+class FormField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    }
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange.bind(this)} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 var App = () => (
   <div>
-    <h2>My Todo List</h2>
+    <h2>Please Input Name and Message</h2>
+      <div>
+        <FormField />
+      </div>
   </div>
 );
 
