39737cd5f5bd2cf726b3af65b2c3eaafcb2e387a
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-21 20:10:31 -0800

    Hookup form to AJAX but it is not currently posting correctly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
-class FormFields extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       name: '',
-      message: ''
+      message: '',
+      response: ''
     }
   }
 
@@ -15,34 +17,50 @@ class FormFields extends React.Component {
   }
 
   handleSubmit(event) {
-    console.log('submit clicked');
     event.preventDefault();
+    var url = 'http://ec2-13-57-25-101.us-west-1.compute.amazonaws.com:3000/api/hrsf110/greeting';
+    var data = {
+      name: this.state.name,
+      message: this.state.message
+    }
+    $.ajax({
+      type: 'POST',
+      url: url,
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      success: (data) => {
+        this.setState({
+          response: data
+        });
+      },
+      error: function(error) {
+        console.error('failed to submit', error);
+      }
+    })
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <label>
-          Name:
-          <input type="text" name={this.state.name} onChange={this.handleChange.bind(this)} />
-        </label>
-        <label>
-          Message:
-          <input type="text" message={this.state.message} onChange={this.handleChange.bind(this)} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <h2>Server Response:</h2>
+          <div>{this.state.response}</div>
+        <h2>Please Input Name and Message</h2>
+          <div>
+            <form onSubmit={this.handleSubmit.bind(this)}>
+              <label>
+                Name:
+                <input type="text" name={this.state.name} onChange={this.handleChange.bind(this)} />
+              </label>
+              <label>
+                Message:
+                <input type="text" message={this.state.message} onChange={this.handleChange.bind(this)} />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+      </div>
     );
   }
 }
-
-var App = () => (
-  <div>
-    <h2>Please Input Name and Message</h2>
-      <div>
-        <FormFields />
-      </div>
-  </div>
-);
 
 export default App;
